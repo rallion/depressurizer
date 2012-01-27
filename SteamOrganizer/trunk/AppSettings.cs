3f261d7c24ddd535b261e152f33fc50fc5635b4c
rallion <rallion@28b34719-f001-a3b9-ac82-4d38589ded8d>
2012-01-27 23:12:28 +0000

    rewrote settings base class to be reflection-based

--- SteamOrganizer/trunk/AppSettings.cs
+++ SteamOrganizer/trunk/AppSettings.cs
@@ -4,88 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Xml;
 using System.IO;
+using System.Reflection;
 
 namespace Depressurizer {
     class AppSettings {
-
-        protected static AppSettings instance = null;
-        public static AppSettings Instance {
-            get {
-                if( instance == null ) {
-                    instance = new AppSettings();
-                }
-                return instance;
-            }
-        }
 
         protected object threadLock = new object();
 
         protected bool outOfDate = false;
 
-        protected Dictionary<string, string> settingsStore;
-
         public string FilePath;
 
         protected AppSettings() {
-            settingsStore = new Dictionary<string, string>();
-            FilePath = System.Environment.GetFolderPath( Environment.SpecialFolder.ApplicationData ) + @"\Depressurizer\Settings.xml";
+            //FilePath = System.Environment.GetFolderPath( Environment.SpecialFolder.ApplicationData ) + @"\Depressurizer\Settings.xml";
+            FilePath = "Settings.xml";
         }
-
-        protected void Set( string key, string val ) {
-            lock( threadLock ) {
-                if( !settingsStore.ContainsKey( key ) || settingsStore[key] != val ) {
-                    outOfDate = true;
-                }
-                settingsStore[key] = val;
-            }
-        }
-
-        protected void Set( string key, int val ) {
-            Set( key, val.ToString() );
-        }
-
-        protected string Get( string key, string defVal ) {
-            lock( threadLock ) {
-                string val;
-                if( settingsStore.TryGetValue( key, out val ) ) {
-                    return val;
-                }
-                Set( key, defVal );
-                return defVal;
-            }
-        }
-
-        protected int Get( string key, int defVal ) {
-            lock( threadLock ) {
-                string strVal;
-                if( settingsStore.TryGetValue( key, out strVal ) ) {
-                    int val;
-                    if( int.TryParse( strVal, out val ) ) {
-                        return val;
-                    }
-                }
-                Set( key, defVal );
-                return defVal;
-            }
-        }
-
-        /*public void Clear( string key ) {
-            lock( threadLock ) {
-                if( settingsStore.ContainsKey( key ) ) {
-                    settingsStore.Remove( key );
-                }
-            }
-        }*/
 
         public void Save( bool force = false ) {
-            lock( threadLock ) {
-                if( force || outOfDate ) {
+            if( force || outOfDate ) {
+                Type t = this.GetType();
+
+                PropertyInfo[] properties = t.GetProperties();
+                lock( threadLock ) {
                     XmlDocument doc = new XmlDocument();
-                    foreach( KeyValuePair<string, string> setting in settingsStore ) {
-                        XmlElement element = doc.CreateElement( setting.Key );
-                        element.InnerText = setting.Value;
-                        doc.AppendChild( element );
+                    XmlElement config = doc.CreateElement( "config" );
+                    foreach( PropertyInfo pi in properties ) {
+                        XmlElement element = doc.CreateElement( pi.Name );
+                        element.InnerText = pi.GetValue( this, null ).ToString();
+                        config.AppendChild( element );
                     }
+                    doc.AppendChild( config );
                     doc.Save( FilePath );
                     outOfDate = false;
                 }
@@ -94,16 +42,39 @@ namespace Depressurizer {
 
         public void Load() {
             lock( threadLock ) {
-                settingsStore = new Dictionary<string, string>();
+                Type type = this.GetType();
                 XmlDocument doc = new XmlDocument();
-                using( FileStream fStream = File.OpenRead( FilePath ) ) {
-                    doc.Load( fStream );
-                }
-
-                foreach( XmlNode node in doc.ChildNodes ) {
-                    settingsStore[node.Name] = node.InnerText;
+                doc.Load( FilePath );
+                XmlNode configNode = doc.SelectSingleNode( "/config" );
+                foreach( XmlNode node in configNode.ChildNodes ) {
+                    string name = node.Name;
+                    string value = node.InnerText;
+                    PropertyInfo pi = type.GetProperty( name );
+                    if( pi != null ) {
+                        this.SetProperty( pi, value );
+                    }
                 }
                 outOfDate = false;
+            }
+        }
+
+        protected void SetProperty( PropertyInfo propertyInfo, string value ) {
+            try {
+                if( propertyInfo.PropertyType == typeof( string ) ) {
+                    propertyInfo.SetValue( this, value, null );
+                } else if( propertyInfo.PropertyType == typeof( bool ) ) {
+                    bool val;
+                    if( bool.TryParse( value, out val ) ) {
+                        propertyInfo.SetValue( this, val, null );
+                    }
+                } else if( propertyInfo.PropertyType == typeof( int ) ) {
+                    int val;
+                    if( int.TryParse( value, out val ) ) {
+                        propertyInfo.SetValue( this, val, null );
+                    }
+                }
+            } catch {
+                // LOG: Log the error
             }
         }
     }
