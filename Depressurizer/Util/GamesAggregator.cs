eca06e9923b375760b396996287e3bfb372cffa1
Bigfootmech <bigfootmech@gmail.com>
2015-06-04 00:05:51 +0900

    A combination of installed and owned games

---
+++ Depressurizer/Util/GamesAggregator.cs
@@ -0,0 +1,31 @@
+﻿using Depressurizer.Games;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Depressurizer.Util
+{
+    class GamesAggregator
+    {
+        public List<int> Matching(InstalledGames installedGames, GameList ownedGames)
+        {
+            List<int> ownedAndInstalled = new List<int>();
+
+            IEnumerable installedGamesList = installedGames.getInstalledGamesList();
+
+            foreach(int installedGameId in installedGamesList)
+            {
+                if (ownedGames.Games.ContainsKey(installedGameId))
+                {
+                    ownedAndInstalled.Add(installedGameId);
+                }
+            }
+
+            return ownedAndInstalled;
+        }
+
+    }
+}
