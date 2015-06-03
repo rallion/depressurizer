3860fa2e4d33709b79f77e75cf7330fcc321664e
Bigfootmech <bigfootmech@gmail.com>
2015-06-04 00:42:49 +0900

    Adding enum for steam's auto update per-game setting

---
+++ Depressurizer/SteamFileAccess/Enums.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Depressurizer.SteamFileAccess
+{
+    public class Enums
+    {
+        public enum GameAutoUpdateSetting{
+            AlwaysUpToDate,
+            OnlyOnLaunch,
+            HighPriority
+        }
+    }
+}
---
+++ DepressurizerUnitTests/SteamFileAccess/EnumTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Depressurizer.SteamFileAccess;
+
+namespace DepressurizerUnitTests.SteamFileAccess
+{
+    [TestClass]
+    public class EnumTests
+    {
+        [TestMethod]
+        public void AssertEnumsCastCorrectly()
+        {
+            // given
+            Enums.GameAutoUpdateSetting enumValue = Enums.GameAutoUpdateSetting.HighPriority;
+
+            // when 
+            int intValue = (int)enumValue;
+
+            // then
+            Assert.AreEqual(intValue, 2);
+        }
+    }
+}
